3cea4e2c5aac50bb75ed3a34b819d5d17db95f90
arter.tendean.07 <arter.tendean.07@gmail.com>
2024-08-20 15:14:10 +0000

    Reformat the code

--- hahaha/10/index.js
+++ hahaha/10/index.js
@@ -1,20 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.get('/profile',(req,res) => {
-    res.send(`/profile with ${req.method}`)
-})
+app.get("/profile", (req, res) => {
+  res.send(`/profile with ${req.method}`);
+});
 
-app.post('/profile',(req,res) => {
-    res.send(`/profile with ${req.method}`)
-})
-
+app.post("/profile", (req, res) => {
+  res.send(`/profile with ${req.method}`);
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
